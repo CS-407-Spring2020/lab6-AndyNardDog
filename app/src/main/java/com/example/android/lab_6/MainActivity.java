2013b2b976c983cb2aed6edb34390b1c7ad01181
Andy Waldron <awaldron2@wisc.edu>
2020-03-06 15:41:28 -0600

    Completed milestone 1 and almost finished milestone 2.

--- app/src/main/java/com/example/android/lab_6/MainActivity.java
+++ app/src/main/java/com/example/android/lab_6/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.android.lab_6;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 
 // My Project 45917
@@ -16,8 +24,10 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends FragmentActivity {
 
-    private final LatLng mDestinationLatLng = new LatLng(-33.8523341, 151.2106085);
+    private final LatLng mDestinationLatLng = new LatLng(43.0752778, -89.4063554);
     private GoogleMap mMap;
+    private FusedLocationProviderClient client;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +39,38 @@ public class MainActivity extends FragmentActivity {
         map.getMapAsync(googleMap -> {
             mMap = googleMap;
             googleMap.addMarker(new MarkerOptions().position(mDestinationLatLng).title("Destination"));
+            displayMyLocation();
         });
 
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+    }
+
+    private void displayMyLocation() {
+        int permission = ActivityCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if(permission == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        } else {
+            client.getLastLocation()
+                    .addOnCompleteListener(this, task -> {
+                        Location lastLocation = task.getResult();
+                        if(task.isSuccessful() && lastLocation != null) {
+                            mMap.addPolyline(new PolylineOptions().add(
+                                    new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude()), mDestinationLatLng));
+                        }
+                    });
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                displayMyLocation();
+            }
+        }
     }
 }

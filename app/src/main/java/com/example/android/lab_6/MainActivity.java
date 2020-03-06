916f489a48dd49c4b28c1f0bba6adf5ed3ee7ca3
Andy Waldron <awaldron2@wisc.edu>
2020-03-05 19:20:06 -0600

    Finished milestone1.

--- app/src/main/java/com/example/android/lab_6/MainActivity.java
+++ app/src/main/java/com/example/android/lab_6/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.android.lab_6;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+// My Project 45917
+// AIzaSyADvsURsuqczlfS4DU1rHaJSiUOhYDfMIM
+
+public class MainActivity extends FragmentActivity {
+
+    private final LatLng mDestinationLatLng = new LatLng(-33.8523341, 151.2106085);
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment map = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_map);
+
+        map.getMapAsync(googleMap -> {
+            mMap = googleMap;
+            googleMap.addMarker(new MarkerOptions().position(mDestinationLatLng).title("Destination"));
+        });
+
     }
 }
